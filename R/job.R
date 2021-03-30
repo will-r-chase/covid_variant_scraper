60b9b0d54496df4e251950cb2b49d552eb8b7249
Will Chase <willchase@Wills-MBP.fios-router.home>
2021-03-30 17:44:38 -0400

    update dates and percents

--- R/job.R
+++ R/job.R
@@ -6,12 +6,15 @@ library(lubridate)
 
 data <- fromJSON("https://api.outbreak.info/genomics/prevalence-by-location-all-lineages?location_id=USA&other_threshold=0.03&nday_threshold=5&ndays=60&timestamp=18709")
 
+percentize <- function(x)(x * 100)
+
 data_df <- data %>% 
   as.data.frame() %>% 
   filter(ymd(results.date) >= ymd("2021-01-01")) %>%
-  filter(ymd(results.date) <= ymd(today() - 13)) %>% 
+  filter(ymd(results.date) <= ymd(today() - 7)) %>% 
   select(results.lineage, results.date, results.prevalence_rolling) %>%
   pivot_wider(names_from = results.lineage, values_from = results.prevalence_rolling) %>%
+  mutate_if(is.numeric, percentize) %>%
   select(results.date, b.1.1.7, b.1.427, b.1.429, b.1.526, b.1.526.1, b.1.526.2, everything())
 
 write_csv(data_df, "data-raw/data.csv")
