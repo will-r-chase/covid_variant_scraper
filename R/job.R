d7160484a83e42474f2f500113341d3d5e214fb4
Will Chase <willchase@Wills-MBP.fios-router.home>
2021-03-30 14:41:43 -0400

    reorder columns

--- R/job.R
+++ R/job.R
@@ -11,6 +11,7 @@ data_df <- data %>%
   filter(ymd(results.date) >= ymd("2021-01-01")) %>%
   filter(ymd(results.date) <= ymd(today() - 13)) %>% 
   select(results.lineage, results.date, results.prevalence_rolling) %>%
-  pivot_wider(names_from = results.lineage, values_from = results.prevalence_rolling)
+  pivot_wider(names_from = results.lineage, values_from = results.prevalence_rolling) %>%
+  select(results.date, b.1.1.7, b.1.427, b.1.429, b.1.526, b.1.526.1, b.1.526.2, everything())
 
 write_csv(data_df, "data-raw/data.csv")
