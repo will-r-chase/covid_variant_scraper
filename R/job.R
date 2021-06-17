911931f79992584bbadd9006e0835c02156cfbc7
Will Chase <willchase@Wills-MacBook-Pro.local>
2021-06-17 10:03:00 -0400

    rollback date

--- R/job.R
+++ R/job.R
@@ -11,10 +11,10 @@ percentize <- function(x)(x * 100)
 data_df <- data %>% 
   as.data.frame() %>% 
   filter(ymd(results.date) >= ymd("2021-01-01")) %>%
-  filter(ymd(results.date) <= ymd(today() - 10)) %>% 
+  filter(ymd(results.date) <= ymd(today() - 13)) %>% 
   select(results.lineage, results.date, results.prevalence_rolling) %>%
   pivot_wider(names_from = results.lineage, values_from = results.prevalence_rolling) %>%
   mutate_if(is.numeric, percentize) %>%
-  select(results.date, b.1.1.7, b.1.429, b.1.526, b.1.526.1, b.1.526.2, p.1, everything())
+  select(results.date, b.1.1.7, b.1.429, b.1.526, b.1.526.1, b.1.526.2, p.1, b.1.617.2, everything())
 
 write_csv(data_df, "data-raw/data.csv")
